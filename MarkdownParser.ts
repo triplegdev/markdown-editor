d6b6f672f7e2dc5e9ced962e05836ecc13c8488b
Anthony Garcia <triplegdev@gmail.com>
2024-07-16 13:57:42 -0400

    added enum tags and tagtype class

--- MarkdownParser.ts
+++ MarkdownParser.ts
@@ -14,3 +14,39 @@ class HtmlHandler {
         }
     }
 }
+
+enum TagType {
+    Paragraph,
+    Header1,
+    Header2,
+    Header3,
+    HorizontalRule
+}
+
+class TagTypeToHtml {
+    private readonly tagType : Map<TagType, string> = new Map<TagType, string>();
+
+    constructor() {
+        this.tagType.set(TagType.Header1, "h1");
+        this.tagType.set(TagType.Header2, "h2");
+        this.tagType.set(TagType.Header3, "h3");
+        this.tagType.set(TagType.Paragraph, "p");
+        this.tagType.set(TagType.HorizontalRule, "hr");
+    }
+
+    public OpeningTag(tagType : TagType) : string {
+        return this.GetTag(tagType, '<');
+    }
+
+    public ClosingTag(tagType : TagType) : string {
+        return this.GetTag(tagType, '</');
+    }
+
+    private GetTag(tagType : TagType, openingTagPattern : string) : string {
+        let tag = this.tagType.get(tagType);
+        if (tag !== null) {
+            return `${openingTagPattern}${tag}>`;
+        }
+        return `${openingTagPattern}p>`
+    }
+}
